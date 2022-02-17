c0fc468c59b9d88308784ab75127c9d58e20b770
Oyinbo David Bayode <dev.davexoyinbo@gmail.com>
2022-02-17 03:01:21 +0100

    navbar bottom

--- jsbootstrap/tailwind.config.js
+++ jsbootstrap/tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
       screens: {
         xs: "425px",
       },
+      colors: {
+        "raisin-black": "#272838",
+        "medium-aquamarina": "#27CE9C",
+        "purple-navy": {
+          30: "#4E5283",
+          70: "#4E5283",
+          80: "#4E5283",
+        },
+        "majorelle-blue": "#6E44FF",
+        "brink-pink": "#FF5280",
+      },
     },
   },
   plugins: [],
